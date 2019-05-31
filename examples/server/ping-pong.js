562a1db8a23f822b70109d7d77de36c9838219cd
Iain Reid <iainreid382@gmail.com>
2019-05-31 14:13:48 +0100

    Lerna implementation (#21)
    * Complete initial Lerna implementation
    
    * Revert structure changes and add client and server
    
    * Add initial server and client libraries
    
    * Correct syntax for local dependency
    
    * Correct badge URLs
    
    * Restructure project to use Lerna
    
    This is in preparation for the migration of the project to the @chaff
    scope, and the addition of the Drowsy server library.
    
    * Update dependencies to resolve Travis build
    
    * Remove global versioning
    
    * Correct package URLs and dependency structure
    
    * Resolve test failures and warnings

---
+++ examples/server/ping-pong.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const drowsy = require("drowsy");
+const server = require("drowsy/server");
+
+const app = drowsy(server());
+
+app.getPing((req, res) => {
+  res.write("pong");
+  res.end();
+});
+
+app.listen(3000, (server) => {
+  console.log(server.address())
+});
